ec3cafbb836af6f31fd5b172c1797478d3982cea
juesdamo1214 <juesdamo1214@gmail.com>
2023-08-09 23:28:38 +0200

    third

--- News-Homepage/src/App.jsx
+++ News-Homepage/src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header"
+import { MainArticle } from "./components/MainArticle"
 
 function App() {
   return (
     <main className="px-4 pt-6">
     <Header />
+    <MainArticle />
     </main>
   )
 }
