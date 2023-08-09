102c0a95910505f4f5a9e8e195045d6822bfb50f
juesdamo1214 <juesdamo1214@gmail.com>
2023-08-09 20:53:35 +0200

    Second

---
+++ News-Homepage/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo.svg'
+import { Navbar } from './Navbar';
+
+export const Header = () => {
+  return (
+    <header className='flex place-content-between items-center'>
+      <img src={logo} alt="" />
+      <Navbar />
+    </header>
+  );
+};
