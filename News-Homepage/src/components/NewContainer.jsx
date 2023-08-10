43a3444222d9e5685c2397c75e0981161ad1bb74
juesdamo1214 <juesdamo1214@gmail.com>
2023-08-10 15:16:46 +0200

    five

--- News-Homepage/src/components/NewContainer.jsx
+++ News-Homepage/src/components/NewContainer.jsx
@@ -3,7 +3,7 @@ import { NewArticle } from './NewArticle'
 
 export const NewContainer = () => {
   return (
-    <aside className='bg-VeryDarkBlue text-OffWhite py-[28px] px-[20px'>
+    <aside className='bg-VeryDarkBlue text-OffWhite py-[28px] px-[20px]'>
         <h1 className='text-SoftOrange text-4xl font-bold'>New</h1>
         <NewArticle
         title='Hydrogen VS Electric Cars'
